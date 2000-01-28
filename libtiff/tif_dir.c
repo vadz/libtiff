bb84ff6d6a67e34791a90a728618311733925f0d
warmerda <warmerda>
2000-01-28 15:40:00 +0000

    return zero from TIFFCreateDirectory().

--- libtiff/tif_dir.c
+++ libtiff/tif_dir.c
@@ -980,12 +980,14 @@ TIFFSetTagExtender(TIFFExtendProc extender)
 int
 TIFFCreateDirectory(TIFF* tif)
 {
-	TIFFDefaultDirectory(tif);
-	tif->tif_diroff = 0;
-        tif->tif_nextdiroff = 0;
-	tif->tif_curoff = 0;
-	tif->tif_row = (uint32) -1;
-	tif->tif_curstrip = (tstrip_t) -1;
+    TIFFDefaultDirectory(tif);
+    tif->tif_diroff = 0;
+    tif->tif_nextdiroff = 0;
+    tif->tif_curoff = 0;
+    tif->tif_row = (uint32) -1;
+    tif->tif_curstrip = (tstrip_t) -1;
+
+    return 0;
 }
 
 /*
