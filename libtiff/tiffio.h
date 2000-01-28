da224566a3d0f4d5774e27cbe944a4fea9cdd136
warmerda <warmerda>
2000-01-28 21:09:02 +0000

    make toff_t unsigned

--- libtiff/tiffio.h
+++ libtiff/tiffio.h
@@ -63,7 +63,8 @@ typedef	struct tiff TIFF;
  * NB: tsize_t is int32 and not uint32 because some functions
  *     return -1.
  * NB: toff_t is not off_t for many reasons; TIFFs max out at
- *     32-bit file offsets being the most important
+ *     32-bit file offsets being the most important, and to ensure
+ *     that it is unsigned, rather than signed.
  */
 typedef	uint32 ttag_t;		/* directory tag */
 typedef	uint16 tdir_t;		/* directory index */
@@ -72,7 +73,7 @@ typedef	uint32 tstrip_t;	/* strip number */
 typedef uint32 ttile_t;		/* tile number */
 typedef	int32 tsize_t;		/* i/o size in bytes */
 typedef	void* tdata_t;		/* image data ref */
-typedef	int32 toff_t;		/* file offset */
+typedef	uint32 toff_t;		/* file offset */
 
 #if !defined(__WIN32__) && (defined(_WIN32) || defined(WIN32))
 #define __WIN32__
