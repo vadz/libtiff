9b26856264d74eb72526b0d8b5f31c0c1acbae9c
warmerda <warmerda>
2000-02-11 19:28:17 +0000

    fixed passing -1 as cc into TIFFWriteEncodedTiles()

--- libtiff/tif_write.c
+++ libtiff/tif_write.c
@@ -383,7 +383,7 @@ TIFFWriteEncodedTile(TIFF* tif, ttile_t tile, tdata_t data, tsize_t cc)
 	 * done so that callers can pass in some large number
 	 * (e.g. -1) and have the tile size used instead.
 	 */
-	if ( cc > tif->tif_tilesize)
+	if ( cc < 1 || cc > tif->tif_tilesize)
 		cc = tif->tif_tilesize;
 	if (!(*tif->tif_encodetile)(tif, (tidata_t) data, cc, sample))
 		return ((tsize_t) 0);
