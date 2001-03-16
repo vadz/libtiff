d57c96a5a51f098a3d41f41e5eaae75e96596d1c
warmerda <warmerda>
2001-03-16 21:05:35 +0000

    moved copyright flag ... must be in tag order!

--- libtiff/tif_dirinfo.c
+++ libtiff/tif_dirinfo.c
@@ -131,8 +131,6 @@ const TIFFFieldInfo tiffFieldInfo[] = {
 #endif
     { TIFFTAG_SOFTWARE,		-1,-1, TIFF_ASCII,	FIELD_SOFTWARE,
       TRUE,	FALSE,	"Software" },
-    { TIFFTAG_COPYRIGHT,	-1,-1, TIFF_ASCII,	FIELD_COPYRIGHT,
-      TRUE,	FALSE,	"Copyright" },
     { TIFFTAG_DATETIME,		20,20, TIFF_ASCII,	FIELD_DATETIME,
       TRUE,	FALSE,	"DateTime" },
     { TIFFTAG_ARTIST,		-1,-1, TIFF_ASCII,	FIELD_ARTIST,
@@ -257,6 +255,8 @@ const TIFFFieldInfo tiffFieldInfo[] = {
       FIELD_MATRIX_WORLDTOSCREEN,	TRUE,	FALSE,	"MatrixWorldToScreen" },
     { TIFFTAG_PIXAR_MATRIX_WORLDTOCAMERA,	16,16,	TIFF_FLOAT,
        FIELD_MATRIX_WORLDTOCAMERA,	TRUE,	FALSE,	"MatrixWorldToCamera" },
+    { TIFFTAG_COPYRIGHT,	-1,-1, TIFF_ASCII,	FIELD_COPYRIGHT,
+      TRUE,	FALSE,	"Copyright" },
 /* end Pixar tags */
 };
 #define	N(a)	(sizeof (a) / sizeof (a[0]))
