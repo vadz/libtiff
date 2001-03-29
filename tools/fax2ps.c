08497271ecfa74c30af636265d2e34f36a55b624
warmerda <warmerda>
2001-03-29 01:45:43 +0000

    From Helge (libtiff@oldach.net)
    Here's a fix for fax2ps that corrects behaviour for non-Letter paper
    sizes. It fixes two problems:
    
    Without scaling (-S) the fax is now centered on the page size specified
    with -H and/or -W. Before, fax2ps was using an obscure and practially
    useless algorithm to allocate the image relative to Letter sized paper
    which sometime sled to useless whitespace on the paper, while at the
    same time cutting of the faxes printable area at the opposite border.
    
    Second, scaling now preserves aspect ratio, which makes unusual faxes
    (in particular short ones) print properly.

--- tools/fax2ps.c
+++ tools/fax2ps.c
@@ -155,7 +155,7 @@ printTIF(TIFF* tif, int pageNumber)
 {
     uint32 w, h;
     uint16 unit;
-    float xres, yres;
+    float xres, yres, scale = 1.0;
     tstrip_t s, ns;
 
     TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &h);
@@ -178,21 +178,13 @@ printTIF(TIFF* tif, int pageNumber)
 
     printf("%%%%Page: \"%d\" %d\n", pageNumber, pageNumber);
     printf("/$pageTop save def gsave\n");
-    if (scaleToPage) {
-	float yscale = pageHeight / (h/yres);
-	float xscale = pageWidth / (w/xres);
-	printf("%d %d translate\n",
-               (int) (((basePageWidth - pageWidth) * points) * half),
-               (int)((yscale*(h/yres)*points) +
-               (basePageHeight - pageHeight) * points * half)  );
-	printf("%g %g scale\n", (72.*xscale)/xres, -(72.*yscale)/yres);
-    } else {
-	printf("%d %d translate\n",
-               (int) ((basePageWidth - pageWidth) * points * half),
-               (int)((72.*h/yres) +
-               (basePageHeight - pageHeight) * points * half) );
-	printf("%g %g scale\n", 72./xres, -72./yres);
-    }
+    if (scaleToPage)
+        scale = pageHeight / (h/yres) < pageWidth / (w/xres) ?
+            pageHeight / (h/yres) : pageWidth / (w/xres);
+    printf("%g %g translate\n",
+           points * (pageWidth - scale*w/xres) * half,
+           points * (scale*h/yres + (pageHeight - scale*h/yres) * half));
+    printf("%g %g scale\n", points/xres*scale, -points/yres*scale);
     printf("0 setgray\n");
     TIFFSetField(tif, TIFFTAG_FAXFILLFUNC, printruns);
     ns = TIFFNumberOfStrips(tif);
