f5ada0b454dc6d94132a7661d88e5e269b79a466
warmerda <warmerda>
2001-09-09 16:10:37 +0000

    Resolve bug 54 - always use purify safe code

--- libtiff/tif_fax3.c
+++ libtiff/tif_fax3.c
@@ -45,12 +45,6 @@
 #include <stdio.h>
 
 /*
- * NB: define PURIFY if you're using purify and you want
- * to avoid some harmless array bounds complaints that
- * can happen in the _TIFFFax3fillruns routine.
- */
-
-/*
  * Compression+decompression state blocks are
  * derived from this ``base state'' block.
  */
@@ -402,12 +396,8 @@ _TIFFFax3fillruns(u_char* buf, uint32* runs, uint32* erun, uint32 lastx)
 			ZERO(n, cp);
 			run &= 7;
 		    }
-#ifdef PURIFY
 		    if (run)
 			cp[0] &= 0xff >> run;
-#else
-		    cp[0] &= 0xff >> run;
-#endif
 		} else
 		    cp[0] &= ~(_fillmasks[run]>>bx);
 		x += runs[0];
@@ -441,12 +431,8 @@ _TIFFFax3fillruns(u_char* buf, uint32* runs, uint32* erun, uint32 lastx)
 			FILL(n, cp);
 			run &= 7;
 		    }
-#ifdef PURIFY
 		    if (run)
 			cp[0] |= 0xff00 >> run;
-#else
-		    cp[0] |= 0xff00 >> run;
-#endif
 		} else
 		    cp[0] |= _fillmasks[run]>>bx;
 		x += runs[1];
