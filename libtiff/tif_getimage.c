ea9615106edf490edd55c77786c50eb2a48a8faa
warmerda <warmerda>
2002-12-05 17:57:26 +0000

    Removed unused variable.

--- libtiff/tif_getimage.c
+++ libtiff/tif_getimage.c
@@ -476,7 +476,7 @@ gtTileContig(TIFFRGBAImage* img, uint32* raster, uint32 w, uint32 h)
     uint16 orientation;
     uint32 col, row, y, rowstoread, ret = 1;
     uint32 pos;
-    uint32 tw, th, tile_row_size;
+    uint32 tw, th;
     u_char* buf;
     int32 fromskew, toskew;
     uint32 nrow;
