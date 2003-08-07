4c38f205bc4c9182e3af1cc2660122cec77257d2
dron <dron>
2003-08-07 16:42:55 +0000

    Fixed problem with float tags reading; support for printing RATIONAL and BYTE tags added.

--- libtiff/tif_print.c
+++ libtiff/tif_print.c
@@ -520,9 +520,18 @@ TIFFPrintDirectory(TIFF* tif, FILE* fd, long flags)
 
                     for( j = 0; j < value_count; j++ )
                     {
-                        if( fld->field_type == TIFF_SHORT )
+			if( fld->field_type == TIFF_BYTE )
+                            fprintf( fd, "%d",
+                                     (int) ((char *) raw_data)[j] );
+			else if( fld->field_type == TIFF_SHORT )
                             fprintf( fd, "%d",
                                      (int) ((short *) raw_data)[j] );
+                        else if( fld->field_type == TIFF_LONG )
+                            fprintf( fd, "%d",
+                                     (int) ((long *) raw_data)[j] );
+			else if( fld->field_type == TIFF_RATIONAL )
+			    fprintf( fd, "%f",
+				     ((float *) raw_data)[j] );
                         else if( fld->field_type == TIFF_ASCII )
                         {
                             fprintf( fd, "%s",
@@ -534,10 +543,7 @@ TIFFPrintDirectory(TIFF* tif, FILE* fd, long flags)
                                      ((double *) raw_data)[j] );
                         else if( fld->field_type == TIFF_FLOAT )
                             fprintf( fd, "%f",
-                                     (float) ((double *) raw_data)[j] );
-                        else if( fld->field_type == TIFF_LONG )
-                            fprintf( fd, "%d",
-                                     (int) ((long *) raw_data)[j] );
+                                     ((float *) raw_data)[j] );
                         else
                         {
                             fprintf( fd,
