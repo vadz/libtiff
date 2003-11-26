1b96c101b7f2e7c13c5dca1628d4d2af5c0e5f73
rossf <rossf>
2003-11-26 09:05:41 +0000

    Modified for CVS

--- tools/tiff2pdf.c
+++ tools/tiff2pdf.c
@@ -1,12 +1,24 @@
-/*
- * Copyright (c) 1990-1997 Sam Leffler
- * Copyright (c) 1991-1997 Silicon Graphics, Inc.
+/* $Id$
+ *
+ * tiff2pdf - converts a TIFF image to a PDF document
+ *
+ * $Log$
+ * Revision 1.2  2003-11-26 09:05:41  rossf
+ * Modified for CVS
+ *
+ *
+ * "d" - Improved colorimetry support
+ * "c" - Added support for orientation, planar configuration
+ * "b" - Improved JPEG handling
+ * "a" - Written/ported for libtiff
+ *
+ * Copyright (c) 2003 Ross Finlayson
  *
  * Permission to use, copy, modify, distribute, and sell this software and 
  * its documentation for any purpose is hereby granted without fee, provided
  * that (i) the above copyright notices and this permission notice appear in
- * all copies of the software and related documentation, and (ii) the names of
- * Sam Leffler and Silicon Graphics may not be used in any advertising or
+ * all copies of the software and related documentation, and (ii) the name of
+ * Ross Finlayson may not be used in any advertising or
  * publicity relating to the software without the specific, prior written
  * permission of Sam Leffler and Silicon Graphics.
  * 
@@ -14,22 +26,13 @@
  * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY 
  * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  
  * 
- * IN NO EVENT SHALL SAM LEFFLER OR SILICON GRAPHICS BE LIABLE FOR
+ * IN NO EVENT SHALL ROSS FINLAYSON BE LIABLE FOR
  * ANY SPECIAL, INCIDENTAL, INDIRECT OR CONSEQUENTIAL DAMAGES OF ANY KIND,
  * OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
  * WHETHER OR NOT ADVISED OF THE POSSIBILITY OF DAMAGE, AND ON ANY THEORY OF 
  * LIABILITY, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE 
  * OF THIS SOFTWARE.
  */
-
-/*
-	tiff2pdf - converts a TIFF image to a PDF document
-
-	"d" - Improved colorimetry support
-	"c" - Added support for orientation, planar configuration
-	"b" - Improved JPEG handling
-	"a" - Written/ported for libtiff
-  */
 
 #include <stdio.h>
 #include <string.h>
