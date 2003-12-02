fb141cb61f32b8e0b8093b315b2bf8336f330767
dron <dron>
2003-12-02 16:40:01 +0000

    Fixed order of the parameters in TIFFError() function calls as per bug http://bugzilla.remotesensing.org/show_bug.cgi?id=440

--- libtiff/tif_dir.c
+++ libtiff/tif_dir.c
@@ -607,18 +607,18 @@ _TIFFVSetField(TIFF* tif, ttag_t tag, va_list ap)
 	va_end(ap);
 	return (status);
 badvalue:
-	TIFFError(module, "%.1000s: Bad value %d for \"%s\"", v,
-		  tif->tif_name, _TIFFFieldWithTag(tif, tag)->field_name);
+	TIFFError(module, "%.1000s: Bad value %d for \"%s\"",
+		  tif->tif_name, v, _TIFFFieldWithTag(tif, tag)->field_name);
 	va_end(ap);
 	return (0);
 badvalue32:
-	TIFFError(module, "%.1000s: Bad value %ld for \"%s\"", v32,
-		   tif->tif_name, _TIFFFieldWithTag(tif, tag)->field_name);
+	TIFFError(module, "%.1000s: Bad value %ld for \"%s\"",
+		   tif->tif_name, v32, _TIFFFieldWithTag(tif, tag)->field_name);
 	va_end(ap);
 	return (0);
 badvaluedbl:
-	TIFFError(module, "%.1000s: Bad value %f for \"%s\"", d,
-		  tif->tif_name, _TIFFFieldWithTag(tif, tag)->field_name);
+	TIFFError(module, "%.1000s: Bad value %f for \"%s\"",
+		  tif->tif_name, d, _TIFFFieldWithTag(tif, tag)->field_name);
 	va_end(ap);
 	return (0);
 }
