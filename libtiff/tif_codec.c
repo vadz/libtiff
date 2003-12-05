a67751ed22b8478ee9095bd2c29ff542e4ff89dc
rossf <rossf>
2003-12-05 14:10:58 +0000

    Fixed a bug, mea culpa.

--- libtiff/tif_codec.c
+++ libtiff/tif_codec.c
@@ -136,10 +136,15 @@ TIFFIsCODECConfigured(uint16 scheme)
 {
 	const TIFFCodec* codec = TIFFFindCODEC(scheme);
 
-	if(codec == NULL)
-		return 0;
-	if( codec->init != (TIFFInitMethod) _notConfigured)
-		return 1;
+	if(codec == NULL) {
+            return 0;
+        }
+        if(codec->init == NULL) {
+            return 0;
+        }
+	if(codec->init != NotConfigured){
+            return 1;
+        }
 	return 0;
 }
 
