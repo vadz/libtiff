7ab709ff3a4d8ab1f78033aee6d365474a2ccdfd
dron <dron>
2004-01-29 15:39:52 +0000

    Check if the pointer is NULL befor proceed further as per bug http://bugzilla.remotesensing.org/show_bug.cgi?id=474

--- libtiff/tif_win32.c
+++ libtiff/tif_win32.c
@@ -224,27 +224,26 @@ _TIFFfree(tdata_t p)
 tdata_t
 _TIFFrealloc(tdata_t p, tsize_t s)
 {
-  void* pvTmp;
-  tsize_t old=GlobalSize(p);
+        void* pvTmp;
+        tsize_t old;
 
-  if(p==NULL)
-    return ((tdata_t)GlobalAlloc(GMEM_FIXED, s));
+        if(p == NULL)
+                return ((tdata_t)GlobalAlloc(GMEM_FIXED, s));
 
-  if (old>=s)
-    {
-      if ((pvTmp = GlobalAlloc(GMEM_FIXED, s)) != NULL) {
-	CopyMemory(pvTmp, p, s);
-	GlobalFree(p);
-      }
-    }
-  else
-    {
-      if ((pvTmp = GlobalAlloc(GMEM_FIXED, s)) != NULL) {
-	CopyMemory(pvTmp, p, old);
-	GlobalFree(p);
-      }
-    }
-  return ((tdata_t)pvTmp);
+        old = GlobalSize(p);
+
+        if (old>=s) {
+                if ((pvTmp = GlobalAlloc(GMEM_FIXED, s)) != NULL) {
+	                CopyMemory(pvTmp, p, s);
+	                GlobalFree(p);
+                }
+        } else {
+                if ((pvTmp = GlobalAlloc(GMEM_FIXED, s)) != NULL) {
+	                CopyMemory(pvTmp, p, old);
+	                GlobalFree(p);
+                }
+        }
+        return ((tdata_t)pvTmp);
 }
 
 void
