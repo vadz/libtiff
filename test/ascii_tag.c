52fd305733b0f0d79cf3ec22d30e8fda05c8cd30
dron <dron>
2004-09-10 12:59:57 +0000

    Complete this test case.

--- test/ascii_tag.c
+++ test/ascii_tag.c
@@ -45,29 +45,34 @@ static struct Tags {
 	ttag_t		tag;
 	const char	*value;
 } ascii_tags[] = {
-	{ TIFFTAG_DOCUMENTNAME, "Test TIFF image." },
-	{ TIFFTAG_IMAGEDESCRIPTION, "Temporary test image." },
-	{ TIFFTAG_MAKE, "This is not scanned image." },
-	{ TIFFTAG_MODEL, "No scanner." },
-	{ TIFFTAG_PAGENAME, "Test page." },
-	{ TIFFTAG_SOFTWARE, "Libtiff library." },
-	{ TIFFTAG_DATETIME, "September,03 2004." },
+	{ TIFFTAG_DOCUMENTNAME, "Test TIFF image" },
+	{ TIFFTAG_IMAGEDESCRIPTION, "Temporary test image" },
+	{ TIFFTAG_MAKE, "This is not scanned image" },
+	{ TIFFTAG_MODEL, "No scanner" },
+	{ TIFFTAG_PAGENAME, "Test page" },
+	{ TIFFTAG_SOFTWARE, "Libtiff library" },
+	{ TIFFTAG_DATETIME, "2004:09:10 16:09:00" },
 	{ TIFFTAG_ARTIST, "Andrey V. Kiselev" },
-	{ TIFFTAG_HOSTCOMPUTER, "Debian GNU/Linux (Sarge)." },
-	{ TIFFTAG_TARGETPRINTER, "No printer." },
-	{ TIFFTAG_PIXAR_TEXTUREFORMAT, "No texture." },
-	{ TIFFTAG_PIXAR_WRAPMODES, "No wrap." },
-	{ TIFFTAG_COPYRIGHT, "Copyright (c) 2004, Andrey Kiselev." }
+	{ TIFFTAG_HOSTCOMPUTER, "Debian GNU/Linux (Sarge)" },
+	{ TIFFTAG_TARGETPRINTER, "No printer" },
+	{ TIFFTAG_PIXAR_TEXTUREFORMAT, "No texture" },
+	{ TIFFTAG_PIXAR_WRAPMODES, "No wrap" },
+	{ TIFFTAG_COPYRIGHT, "Copyright (c) 2004, Andrey Kiselev" }
 };
 #define NTAGS   (sizeof (ascii_tags) / sizeof (ascii_tags[0]))
+
+const char *ink_names = "Red\0Green\0Blue";
+const int ink_names_size = 15;
 
 int
 main(int argc, char **argv)
 {
 	TIFF		*tif;
-	unsigned char	buf[1] = { 255 };
 	int		i;
+	unsigned char	buf[3] = { 0, 127, 255 };
+	char		*value;
 
+	/* Test whether we can write tags. */
 	tif = TIFFOpen(filename, "w");
 	if (!tif) {
 		fprintf (stderr, "Can't create test TIFF file %s.\n", filename);
@@ -76,27 +81,27 @@ main(int argc, char **argv)
 
 	if (!TIFFSetField(tif, TIFFTAG_IMAGEWIDTH, 1)) {
 		fprintf (stderr, "Can't set ImageWidth tag.\n");
-		goto bad;
+		goto failure;
 	}
 	if (!TIFFSetField(tif, TIFFTAG_IMAGELENGTH, 1)) {
 		fprintf (stderr, "Can't set ImageLength tag.\n");
-		goto bad;
+		goto failure;
 	}
 	if (!TIFFSetField(tif, TIFFTAG_BITSPERSAMPLE, 8)) {
 		fprintf (stderr, "Can't set BitsPerSample tag.\n");
-		goto bad;
+		goto failure;
 	}
-	if (!TIFFSetField(tif, TIFFTAG_SAMPLESPERPIXEL, 1)) {
+	if (!TIFFSetField(tif, TIFFTAG_SAMPLESPERPIXEL, 3)) {
 		fprintf (stderr, "Can't set SamplesPerPixel tag.\n");
-		goto bad;
+		goto failure;
 	}
 	if (!TIFFSetField(tif, TIFFTAG_PLANARCONFIG, PLANARCONFIG_CONTIG)) {
 		fprintf (stderr, "Can't set PlanarConfiguration tag.\n");
-		goto bad;
+		goto failure;
 	}
-	if (!TIFFSetField(tif, TIFFTAG_PHOTOMETRIC, PHOTOMETRIC_MINISBLACK)) {
+	if (!TIFFSetField(tif, TIFFTAG_PHOTOMETRIC, PHOTOMETRIC_RGB)) {
 		fprintf (stderr, "Can't set PhotometricInterpretation tag.\n");
-		goto bad;
+		goto failure;
 	}
 
 	for (i = 0; i < NTAGS; i++) {
@@ -104,20 +109,58 @@ main(int argc, char **argv)
 				  ascii_tags[i].value)) {
 			fprintf (stderr, "Can't set tag %d.\n",
 				 ascii_tags[i].tag);
-			goto bad;
+			goto failure;
 		}
 	}
 
+	/* InkNames tag has special form, so we handle it separately. */
+	if (!TIFFSetField(tif, TIFFTAG_NUMBEROFINKS, 3)) {
+		fprintf (stderr, "Can't set tag %d.\n", TIFFTAG_NUMBEROFINKS);
+		goto failure;
+	}
+	if (!TIFFSetField(tif, TIFFTAG_INKNAMES, ink_names_size, ink_names)) {
+		fprintf (stderr, "Can't set tag %d.\n", TIFFTAG_INKNAMES);
+		goto failure;
+	}
+
+	/* Write dummy pixel data. */
 	if (!TIFFWriteScanline(tif, buf, 0, 0) < 0) {
 		fprintf (stderr, "Can't write image data.\n");
-		goto bad;
+		goto failure;
 	}
 
 	TIFFClose(tif);
+	
+	/* Ok, now test whether we can read written values. */
+	tif = TIFFOpen(filename, "r");
+	if (!tif) {
+		fprintf (stderr, "Can't open test TIFF file %s.\n", filename);
+		return 1;
+	}
+
+	for (i = 0; i < NTAGS; i++) {
+		if (!TIFFGetField(tif, ascii_tags[i].tag, &value)
+		    && !strcmp(value, ascii_tags[i].value)) {
+			fprintf (stderr, "Can't get tag %d.\n",
+				 ascii_tags[i].tag);
+			goto failure;
+		}
+	}
+
+	if (!TIFFGetField(tif, TIFFTAG_INKNAMES, &value)
+	    && !memcmp(value, ink_names, ink_names_size)) {
+		fprintf (stderr, "Can't get tag %d.\n", TIFFTAG_INKNAMES);
+		goto failure;
+	}
+
+	TIFFClose(tif);
+	
+	/* All tests passed; delete file and exit with success status. */
 	unlink(filename);
 	return 0;
 
-bad:
+failure:
+	/* Something goes wrong; close file and return unsuccessful status. */
 	TIFFClose(tif);
 	unlink(filename);
 	return 1;
