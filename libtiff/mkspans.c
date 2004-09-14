b76859c4d5f060623df82bf9a8fbf692776e6619
dron <dron>
2004-09-14 05:45:46 +0000

    Get rid of BSD data types (u_char, u_short, u_int, u_long).

--- libtiff/mkspans.c
+++ libtiff/mkspans.c
@@ -1,4 +1,4 @@
-/* $Header$ */
+/* $Id$ */
 
 /*
  * Copyright (c) 1991-1997 Sam Leffler
@@ -37,7 +37,7 @@ dumparray(name, runs)
 {
 	register int i;
 	register char *sep;
-	printf("static u_char %s[256] = {\n", name);
+	printf("static unsigned char %s[256] = {\n", name);
 	sep = "    ";
 	for (i = 0; i < 256; i++) {
 		printf("%s%d", sep, runs[i]);
@@ -70,3 +70,5 @@ main()
 	dumparray("bruns", runs[0]);
 	dumparray("wruns", runs[1]);
 }
+
+/* vim: set ts=8 sts=8 sw=8 noet: */
