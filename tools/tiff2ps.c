1ba9142d637d7dd997f4716e4b09ab34efb9cbcd
dron <dron>
2004-10-30 13:46:33 +0000

    Fixed wrong variable data type when read Position tags (Tristan Hill).

--- tools/tiff2ps.c
+++ tools/tiff2ps.c
@@ -542,7 +542,8 @@ TIFF2PS(FILE* fd, TIFF* tif,
 	double pw, double ph, double lm, double bm, int cnt)
 {
 	uint32 w, h;
-	double ox, oy, prw, prh;
+	float ox, oy;
+        double prw, prh;
 	double scale = 1.0;
 	double left_offset = lm * PS_UNIT_SIZE;
 	double bottom_offset = bm * PS_UNIT_SIZE;
