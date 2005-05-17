bd56762267d7e67f89d31dedcddf7dca9e003af3
dron <dron>
2005-05-17 14:18:48 +0000

    Check for JPEGTables tag presence before copying.

--- tools/tiffsplit.c
+++ tools/tiffsplit.c
@@ -151,10 +151,12 @@ tiffcp(TIFF* in, TIFF* out)
 	CopyField(TIFFTAG_SAMPLESPERPIXEL, samplesperpixel);
 	CopyField(TIFFTAG_COMPRESSION, compression);
 	if (compression == COMPRESSION_JPEG) {
-		uint16 count;
-		void *table;
-		TIFFGetField(in, TIFFTAG_JPEGTABLES, &count, &table);
-		TIFFSetField(out, TIFFTAG_JPEGTABLES, count, table);
+		uint16 count = 0;
+		void *table = NULL;
+		if (TIFFGetField(in, TIFFTAG_JPEGTABLES, &count, &table)
+		    && count > 0 && table) {
+		    TIFFSetField(out, TIFFTAG_JPEGTABLES, count, table);
+		}
 	}
         CopyField(TIFFTAG_PHOTOMETRIC, shortv);
 	CopyField(TIFFTAG_PREDICTOR, shortv);
