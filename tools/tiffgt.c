d95d79c91a9cefe4cf794c7745bc9b60b16d1bb1
dron <dron>
2005-06-23 11:50:05 +0000

    Use MacOS X OpenGL framework when appropriate. As per bug http://bugzilla.remotesensing.org/show_bug.cgi?id=844

--- tools/tiffgt.c
+++ tools/tiffgt.c
@@ -31,10 +31,19 @@
 #include <string.h>
 #include <unistd.h>
 
-#include <GL/gl.h>
-#include <GL/glut.h>
+#if HAVE_APPLE_OPENGL_FRAMEWORK
+# include <OpenGL/gl.h>
+# include <GLUT/glut.h>
+#else
+# include <GL/gl.h>
+# include <GL/glut.h>
+#endif
 
 #include "tiffio.h"
+
+#ifndef HAVE_GETOPT
+extern int getopt(int, char**, char*);
+#endif
 
 static	uint32	width = 0, height = 0;		/* window width & height */
 static	uint32*	raster = NULL;			/* displayable image */
