0bf16a6146aeb83bd31a4a78e8836e485f06dd2a
dron <dron>
2005-07-25 14:22:27 +0000

    Fixed printing of the BYTE and SBYTE arrays.

--- libtiff/tif_print.c
+++ libtiff/tif_print.c
@@ -466,8 +466,10 @@ TIFFPrintDirectory(TIFF* tif, FILE* fd, long flags)
 					_TIFFDataSize(fip->field_type)
 					* value_count);
 				mem_alloc = 1;
-				if(TIFFGetField(tif, tag, raw_data) != 1)
+				if(TIFFGetField(tif, tag, raw_data) != 1) {
+					_TIFFfree(raw_data);
 					continue;
+				}
 			}
 		}
 
@@ -475,29 +477,26 @@ TIFFPrintDirectory(TIFF* tif, FILE* fd, long flags)
 
 		for(j = 0; j < value_count; j++) {
 		    if(fip->field_type == TIFF_BYTE)
-		        fprintf(fd, "%u",
-				(unsigned int) ((uint16 *) raw_data)[j]);
+		        fprintf(fd, "%u", ((uint8 *) raw_data)[j]);
 		    else if(fip->field_type == TIFF_UNDEFINED)
 		        fprintf(fd, "0x%x",
 				(unsigned int) ((unsigned char *) raw_data)[j]);		    else if(fip->field_type == TIFF_SBYTE)
-		        fprintf(fd, "%d", (int) ((uint16 *) raw_data)[j]);
+		        fprintf(fd, "%d", ((int8 *) raw_data)[j]);
 		    else if(fip->field_type == TIFF_SHORT)
-		        fprintf(fd, "%u",
-		    	    (unsigned int)((unsigned short *) raw_data)[j]);
+		        fprintf(fd, "%u", ((uint16 *) raw_data)[j]);
 		    else if(fip->field_type == TIFF_SSHORT)
-		        fprintf(fd, "%d", (int)((short *) raw_data)[j]);
+		        fprintf(fd, "%d", ((int16 *) raw_data)[j]);
 		    else if(fip->field_type == TIFF_LONG)
 		        fprintf(fd, "%lu",
-		    	    (unsigned long)((unsigned long *) raw_data)[j]);
+		    	    (unsigned long)((uint32 *) raw_data)[j]);
 		    else if(fip->field_type == TIFF_SLONG)
-		        fprintf(fd, "%ld", (long)((long *) raw_data)[j]);
+		        fprintf(fd, "%ld", (long)((int32 *) raw_data)[j]);
 		    else if(fip->field_type == TIFF_RATIONAL
 			    || fip->field_type == TIFF_SRATIONAL
 			    || fip->field_type == TIFF_FLOAT)
 		        fprintf(fd, "%f", ((float *) raw_data)[j]);
 		    else if(fip->field_type == TIFF_IFD)
-		        fprintf(fd, "0x%x",
-		    	    (int)((unsigned long *) raw_data)[j]);
+		        fprintf(fd, "0x%lx", ((uint32 *) raw_data)[j]);
 		    else if(fip->field_type == TIFF_ASCII) {
 		        fprintf(fd, "%s", (char *) raw_data);
 		        break;
