bcce2a443ba254879762c152b7e60dadbda3a91a
dron <dron>
2005-09-28 12:10:22 +0000

    Fixed crash when reading malformed tags.

--- tools/tiffdump.c
+++ tools/tiffdump.c
@@ -702,8 +702,8 @@ TIFFFetchData(int fd, TIFFDirEntry* dir, void* cp)
 
 	w = (dir->tdir_type < NWIDTHS ? datawidth[dir->tdir_type] : 0);
 	cc = dir->tdir_count * w;
-	if (lseek(fd, (off_t)dir->tdir_offset, 0) == (off_t)dir->tdir_offset &&
-	    read(fd, cp, cc) == cc) {
+	if (lseek(fd, (off_t)dir->tdir_offset, 0) != (off_t)-1
+	    && read(fd, cp, cc) != -1) {
 		if (swabflag) {
 			switch (dir->tdir_type) {
 			case TIFF_SHORT:
