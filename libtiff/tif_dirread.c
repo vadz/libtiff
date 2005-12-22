3edb9cda25391dd2345da41e05de602960a66ebd
dron <dron>
2005-12-22 07:13:36 +0000

    Fix in StribByteCount guessing code.

--- libtiff/tif_dirread.c
+++ libtiff/tif_dirread.c
@@ -586,7 +586,7 @@ TIFFReadDirectory(TIFF* tif)
 		            _TIFFFieldWithTag(tif,TIFFTAG_STRIPBYTECOUNTS)->field_name);
 		if(EstimateStripByteCounts(tif, dir, dircount) < 0)
 		    goto bad;
-	} else if (td->td_nstrips > 1
+	} else if (td->td_nstrips > 2
 		   && td->td_compression == COMPRESSION_NONE
 		   && td->td_stripbytecount[0] != td->td_stripbytecount[1]) {
 		/*
