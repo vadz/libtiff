97e9301645b517de84800c8dbb0ea28a0aa9e096
joris <joris>
2006-04-08 02:15:40 +0000

    resolved memory leak in tif_ojpeg

--- libtiff/tif_ojpeg.c
+++ libtiff/tif_ojpeg.c
@@ -1106,7 +1106,7 @@ OJPEGLibjpegSessionAbort(TIFF* tif)
 {
 	OJPEGState* sp=(OJPEGState*)tif->tif_data;
 	assert(sp->libjpeg_session_active!=0);
-	jpeg_abort((jpeg_common_struct*)(&(sp->libjpeg_jpeg_decompress_struct)));
+	jpeg_destroy((jpeg_common_struct*)(&(sp->libjpeg_jpeg_decompress_struct)));
 	sp->libjpeg_session_active=0;
 }
 
