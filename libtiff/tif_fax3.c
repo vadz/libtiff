3fba5ddab1963901516a4802653fbc092540af5a
fwarmerdam <fwarmerdam>
2007-03-28 21:47:01 +0000

    fix static/inline issue with IRIC C++ Compiler

--- libtiff/tif_fax3.c
+++ libtiff/tif_fax3.c
@@ -776,7 +776,7 @@ static	int32 find1span(unsigned char*, int32, int32);
  * table.  The ``base'' of the bit string is supplied
  * along with the start+end bit indices.
  */
-inline static int32
+static inline int32
 find0span(unsigned char* bp, int32 bs, int32 be)
 {
 	int32 bits = be - bs;
@@ -835,7 +835,7 @@ find0span(unsigned char* bp, int32 bs, int32 be)
 	return (span);
 }
 
-inline static int32
+static inline int32
 find1span(unsigned char* bp, int32 bs, int32 be)
 {
 	int32 bits = be - bs;
