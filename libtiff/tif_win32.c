8ea5ee1006aa6f5322e8b9157e3e9febf80596c5
fwarmerdam <fwarmerdam>
2008-01-01 15:35:54 +0000

    modify CreateFileMapping() for win64

--- libtiff/tif_win32.c
+++ libtiff/tif_win32.c
@@ -164,7 +164,10 @@ _tiffMapProc(thandle_t fd, void** pbase, toff_t* psize)
 	sizem = (tmsize_t)size;
 	if ((uint64)sizem!=size)
 		return (0);
-	hMapFile = CreateFileMapping(fd, NULL, PAGE_READONLY, 0, sizem, NULL);
+
+	// By passing in 0 for the maximum file size, it specifies that we
+	// create a file mapping object for the full file size.
+	hMapFile = CreateFileMapping(fd, NULL, PAGE_READONLY, 0, 0, NULL);
 	if (hMapFile == NULL)
 		return (0);
 	*pbase = MapViewOfFile(hMapFile, FILE_MAP_READ, 0, 0, 0);
