398a3b6c1be443b4333f4cc839d24160d9abcd21
fwarmerdam <fwarmerdam>
2008-05-25 03:10:55 +0000

    avoid possible NULL in error

--- libtiff/tif_codec.c
+++ libtiff/tif_codec.c
@@ -102,9 +102,12 @@ static int
 _notConfigured(TIFF* tif)
 {
 	const TIFFCodec* c = TIFFFindCODEC(tif->tif_dir.td_compression);
-
+        char compression_code[20];
+        
+        sprintf( compression_code, "%d", tif->tif_dir.td_compression );
 	TIFFErrorExt(tif->tif_clientdata, tif->tif_name,
-	    "%s compression support is not configured", c->name);
+                     "%s compression support is not configured", 
+                     c ? c->name : compression_code );
 	return (0);
 }
 
