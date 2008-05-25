b84bfc6f8fe8eb7bd55b5080766ab1b0a91f2ba7
fwarmerdam <fwarmerdam>
2008-05-25 01:53:09 +0000

    zero tif->tif_dir after freeing the directory in TIFFReadCustomDirectory(). I don't exactly remember why this was important. --- Fix unchecked malloc result.

--- libtiff/tif_dirread.c
+++ libtiff/tif_dirread.c
@@ -782,6 +782,7 @@ TIFFReadCustomDirectory(TIFF* tif, toff_t diroff,
 	}
 
 	TIFFFreeDirectory(tif);
+        _TIFFmemset(&tif->tif_dir, 0, sizeof(TIFFDirectory));
 
 	fix = 0;
 	for (dp = dir, i = dircount; i > 0; i--, dp++) {
@@ -905,6 +906,9 @@ EstimateStripByteCounts(TIFF* tif, TIFFDirEntry* dir, uint16 dircount)
 	td->td_stripbytecount = (uint32*)
 	    _TIFFCheckMalloc(tif, td->td_nstrips, sizeof (uint32),
 		"for \"StripByteCounts\" array");
+        if( td->td_stripbytecount == NULL )
+            return -1;
+
 	if (td->td_compression != COMPRESSION_NONE) {
 		uint32 space = (uint32)(sizeof (TIFFHeader)
 		    + sizeof (uint16)
