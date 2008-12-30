362dee59d20f4e2b081c16b249dec28e8ac8c57e
bfriesen <bfriesen>
2008-12-30 21:59:54 +0000

    Don't throw away output to stderr.  Collect and display it if there is an error.

--- test/tiffcp-thumbnail.sh
+++ test/tiffcp-thumbnail.sh
@@ -6,14 +6,15 @@
 
 outfile1=deleteme-in-$$.tif
 outfile2=deleteme-out-$$.tif
+stderr=deleteme-stderr-$$.tif
 operation=tiffcp
-${TIFFCP} -c g3:1d ${IMG_MINISWHITE_1C_1B} ${outfile1}
+${TIFFCP} -c g3:1d ${IMG_MINISWHITE_1C_1B} ${outfile1} 2> $stderr
 status=$?
 
 if test $status -eq 0
 then
   operation=thumbnail
-  ${THUMBNAIL} ${outfile1} ${outfile2} 2> /dev/null 
+  ${THUMBNAIL} ${outfile1} ${outfile2} 2>> $stderr
   status=$?
 fi
 
@@ -21,8 +22,11 @@ if test $status -eq 0
 then
   rm -f ${outfile1} ${outfile2}
 else
+  cat $stderr
   echo "Test failed (${operation} returned ${status}).  Please inspect these output files:"
   echo "  " ${outfile1} ${outfile2}
 fi
+
+rm -f $stderr
 
 exit $status
