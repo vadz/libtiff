277e54fb5c329932ed3e705a1795ce3b85c8b0c7
fwarmerdam <fwarmerdam>
2009-01-21 04:49:44 +0000

    tiffsplit.c: fix sampleformat to be shortv instead of longv

--- tools/tiffsplit.c
+++ tools/tiffsplit.c
@@ -197,7 +197,7 @@ tiffcp(TIFF* in, TIFF* out)
 	CopyField(TIFFTAG_YPOSITION, floatv);
 	CopyField(TIFFTAG_IMAGEDEPTH, longv);
 	CopyField(TIFFTAG_TILEDEPTH, longv);
-	CopyField(TIFFTAG_SAMPLEFORMAT, longv);
+	CopyField(TIFFTAG_SAMPLEFORMAT, shortv);
 	CopyField2(TIFFTAG_EXTRASAMPLES, shortv, shortav);
 	{ uint16 *red, *green, *blue;
 	  CopyField3(TIFFTAG_COLORMAP, red, green, blue);
