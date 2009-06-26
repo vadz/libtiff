12d28d121b5615392ac1d36678ae83cf740d5c34
fwarmerdam <fwarmerdam>
2009-06-26 20:55:54 +0000

    Remove an inappropriate assertion that often fails on oddly sized 12bit jpeg compressed ycbcr images.

--- libtiff/tif_strip.c
+++ libtiff/tif_strip.c
@@ -327,12 +327,11 @@ TIFFScanlineSize64(TIFF* tif)
                                          "Invalid YCbCr subsampling");
                             return 0;
 			}
-			samplingblock_samples=ycbcrsubsampling[0]*ycbcrsubsampling[1]+2;
-			samplingblocks_hor=TIFFhowmany_32(td->td_imagewidth,ycbcrsubsampling[0]);
-			samplingrow_samples=multiply_64(tif,samplingblocks_hor,samplingblock_samples,module);
-			samplingrow_size=TIFFhowmany_64(multiply_64(tif,samplingrow_samples,td->td_bitspersample,module),8);
-			assert((samplingrow_size%ycbcrsubsampling[1])==0);
-			scanline_size=(samplingrow_size/ycbcrsubsampling[1]);
+			samplingblock_samples = ycbcrsubsampling[0]*ycbcrsubsampling[1]+2;
+			samplingblocks_hor = TIFFhowmany_32(td->td_imagewidth,ycbcrsubsampling[0]);
+			samplingrow_samples = multiply_64(tif,samplingblocks_hor,samplingblock_samples,module);
+			samplingrow_size = TIFFhowmany_64(multiply_64(tif,samplingrow_samples,td->td_bitspersample,module),8);
+			scanline_size = (samplingrow_size/ycbcrsubsampling[1]);
 		}
 		else
 		{
