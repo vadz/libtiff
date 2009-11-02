21887614655a78d694b1147318ea65c2d7a8c51e
bfriesen <bfriesen>
2009-11-02 18:30:21 +0000

    Change tiffcrop parameters to -X 60 -Y 60

--- test/tiffcrop-extract-minisblack-2c-8b-alpha.sh
+++ test/tiffcrop-extract-minisblack-2c-8b-alpha.sh
@@ -3,5 +3,5 @@
 . ${srcdir:-.}/common.sh
 infile="$srcdir/images/minisblack-2c-8b-alpha.tiff"
 outfile="o-tiffcrop-extract-minisblack-2c-8b-alpha.tiff"
-f_test_convert "$TIFFCROP -U px -E top -X 100 -Y 100" $infile $outfile
+f_test_convert "$TIFFCROP -U px -E top -X 60 -Y 60" $infile $outfile
 f_tiffinfo_validate $outfile
