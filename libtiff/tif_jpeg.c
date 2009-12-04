b0d74930fed1a6f5a9a228110b19a84f22165583
fwarmerdam <fwarmerdam>
2009-12-04 01:04:00 +0000

    1) Reset tif->tif_curstrip to -1 at the end of the JPEGFixupTestSubsampling() so that JPEGPreDecode will be called again later when the first scanline or block is read.  This ensures that the appropriate decode method gets installed. 2) In JPEGResetUpsampling() the recomputation of tif_scanlinesize size is triggered (if we already had a meaningful value).  This fix was actually provided by Even Rouault this afternoon. http://bugzilla.maptools.org/show_bug.cgi?id=1936

--- libtiff/tif_jpeg.c
+++ libtiff/tif_jpeg.c
@@ -1613,7 +1613,11 @@ JPEGResetUpsampled( TIFF* tif )
 	 * Must recalculate cached tile size in case sampling state changed.
 	 * Should we really be doing this now if image size isn't set? 
 	 */
-	tif->tif_tilesize = isTiled(tif) ? TIFFTileSize(tif) : (tsize_t) -1;
+        if( tif->tif_tilesize > 0 )
+            tif->tif_tilesize = isTiled(tif) ? TIFFTileSize(tif) : (tsize_t) -1;
+
+        if(tif->tif_scanlinesize > 0 )
+            tif->tif_scanlinesize = TIFFScanlineSize(tif); 
 }
 
 static int
@@ -1741,13 +1745,21 @@ JPEGFixupTestSubsampling( TIFF * tif )
 			return;
     }
     else
-	{
+    {
         if( !TIFFFillStrip( tif, 0 ) )
             return;
     }
 
     TIFFSetField( tif, TIFFTAG_YCBCRSUBSAMPLING, 
                   (uint16) sp->h_sampling, (uint16) sp->v_sampling );
+
+    /*
+    ** We want to clear the loaded strip so the application has time
+    ** to set JPEGCOLORMODE or other behavior modifiers.  This essentially
+    ** undoes the JPEGPreDecode triggers by TIFFFileStrip().  (#1936)
+    */
+    tif->tif_curstrip = -1;
+
 #endif /* CHECK_JPEG_YCBCR_SUBSAMPLING */
 }
 
