a3ff4e987dabf9eda216167d0a5aee50252d6cac
faxguy <faxguy>
2010-02-22 19:24:39 +0000

    * libtiff/tif_fax3.c: Don't return error on badly-terminated MMR         strips.         http://bugzilla.maptools.org/show_bug.cgi?id=2029

--- libtiff/tif_fax3.c
+++ libtiff/tif_fax3.c
@@ -1397,12 +1397,12 @@ Fax4Decode(TIFF* tif, uint8* buf, tmsize_t occ, uint16 s)
         BADG4:
 #ifdef FAX3_DEBUG
                 if( GetBits(13) != 0x1001 )
-                    fputs( "Bad RTC\n", stderr );
+                    fputs( "Bad EOFB\n", stderr );
 #endif                
                 ClrBits( 13 );
 		(*sp->fill)(buf, thisrun, pa, lastx);
 		UNCACHE_STATE(tif, sp);
-		return (-1);
+		return ( line ? 1 : -1);	/* don't error on badly-terminated strips */
 	}
 	UNCACHE_STATE(tif, sp);
 	return (1);
