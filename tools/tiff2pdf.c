96a5fb4c8ffc9f9a5f78071b80ddec72861f7a32
faxguy <faxguy>
2010-12-13 05:41:38 +0000

    * tools/tiff2pdf.c: fix colors for images with RGBA         interleaved data         http://bugzilla.maptools.org/show_bug.cgi?id=2250

--- tools/tiff2pdf.c
+++ tools/tiff2pdf.c
@@ -3482,11 +3482,10 @@ t2p_sample_rgba_to_rgb(tdata_t data, uint32 samplecount)
 	
 	for (i = 0; i < samplecount; i++) {
 		sample=((uint32*)data)[i];
-		alpha=(uint8)((255 - (sample & 0xff)));
-		((uint8 *)data)[i * 3] = (uint8) ((sample >> 24) & 0xff) + alpha;
-		((uint8 *)data)[i * 3 + 1] = (uint8) ((sample >> 16) & 0xff) + alpha;
-		((uint8 *)data)[i * 3 + 2] = (uint8) ((sample >> 8) & 0xff) + alpha;
-		
+		alpha=(uint8)((255 - ((sample >> 24) & 0xff)));
+		((uint8 *)data)[i * 3] = (uint8) ((sample >> 16) & 0xff) + alpha;
+		((uint8 *)data)[i * 3 + 1] = (uint8) ((sample >> 8) & 0xff) + alpha;
+		((uint8 *)data)[i * 3 + 2] = (uint8) (sample & 0xff) + alpha;
 	}
 
 	return (i * 3);
