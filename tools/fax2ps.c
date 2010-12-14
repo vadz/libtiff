d91c393e9c7255561035a3b8ca0889e8dceed306
faxguy <faxguy>
2010-12-14 03:16:45 +0000

    * tools/fax2ps.c: be consistent with page-numbering         http://bugzilla.maptools.org/show_bug.cgi?id=2225

--- tools/fax2ps.c
+++ tools/fax2ps.c
@@ -277,9 +277,9 @@ findPage(TIFF* tif, uint16 pageNumber)
     uint16 pn = (uint16) -1;
     uint16 ptotal = (uint16) -1;
     if (GetPageNumber(tif)) {
-	while (pn != pageNumber && TIFFReadDirectory(tif) && GetPageNumber(tif))
+	while (pn != (pageNumber-1) && TIFFReadDirectory(tif) && GetPageNumber(tif))
 	    ;
-	return (pn == pageNumber);
+	return (pn == (pageNumber-1));
     } else
 	return (TIFFSetDirectory(tif, (tdir_t)(pageNumber-1)));
 }
