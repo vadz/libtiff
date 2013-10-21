b96fdc3e5169c4540cb94d3331ef6bcf8bfed1f4
fwarmerdam <fwarmerdam>
2013-10-21 18:23:48 +0000

    Error out on directory counting overflow.

--- libtiff/tif_dir.c
+++ libtiff/tif_dir.c
@@ -1471,6 +1471,7 @@ TIFFAdvanceDirectory(TIFF* tif, uint64* nextdir, uint64* off)
 uint16
 TIFFNumberOfDirectories(TIFF* tif)
 {
+	static const char module[] = "TIFFNumberOfDirectories";
 	uint64 nextdir;
 	uint16 n;
 	if (!(tif->tif_flags&TIFF_BIGTIFF))
@@ -1479,7 +1480,14 @@ TIFFNumberOfDirectories(TIFF* tif)
 		nextdir = tif->tif_header.big.tiff_diroff;
 	n = 0;
 	while (nextdir != 0 && TIFFAdvanceDirectory(tif, &nextdir, NULL))
-		n++;
+        {
+		if(n++ == 0)
+                {
+                        TIFFErrorExt(tif->tif_clientdata, module,
+                                     "Directory count exceeded 65535 limit, giving up on counting.");
+                        return (65535);
+                }
+        }
 	return (n);
 }
 
