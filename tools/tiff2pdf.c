a77e2f6ed17b8cc25a15aa3888e4d53cdef393c1
olivier <olivier>
2015-01-05 19:03:11 +0000

    * tools/tiff2pdf.c: Fixed unsigned integer addition overflow detection.

--- tools/tiff2pdf.c
+++ tools/tiff2pdf.c
@@ -440,7 +440,7 @@ checkAdd64(uint64 summand1, uint64 summand2, T2P* t2p)
 {
 	uint64 bytes = summand1 + summand2;
 
-	if (bytes - summand1 != summand2) {
+	if (bytes < summand1) {
 		TIFFError(TIFF2PDF_MODULE, "Integer overflow");
 		t2p->t2p_error = T2P_ERR_ERROR;
 		bytes = 0;
