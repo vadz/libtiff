e87a8bccf1c23f2c63d36fb8ac5758b499aa3711
bfriesen <bfriesen>
2015-05-28 01:59:10 +0000

    (readContigStripsIntoBuffer): Fix Coverity 1024545 "Division or modulo by zero".

--- tools/tiffcrop.c
+++ tools/tiffcrop.c
@@ -3599,34 +3599,38 @@ extractContigSamplesToTileBuffer(uint8 *out, uint8 *in, uint32 rows, uint32 cols
   } /* end extractContigSamplesToTileBuffer */
 
 static int readContigStripsIntoBuffer (TIFF* in, uint8* buf)
-  {
-  uint8* bufp = buf;
-  int32  bytes_read = 0;
-  uint16 strip, nstrips   = TIFFNumberOfStrips(in);
-  uint32 stripsize = TIFFStripSize(in);
-  uint32 rows = 0;
-  uint32 rps = TIFFGetFieldDefaulted(in, TIFFTAG_ROWSPERSTRIP, &rps);
-  tsize_t scanline_size = TIFFScanlineSize(in);
+{
+        uint8* bufp = buf;
+        int32  bytes_read = 0;
+        uint16 strip, nstrips   = TIFFNumberOfStrips(in);
+        uint32 stripsize = TIFFStripSize(in);
+        uint32 rows = 0;
+        uint32 rps = TIFFGetFieldDefaulted(in, TIFFTAG_ROWSPERSTRIP, &rps);
+        tsize_t scanline_size = TIFFScanlineSize(in);
 
-  for (strip = 0; strip < nstrips; strip++)
-    {
-    bytes_read = TIFFReadEncodedStrip (in, strip, bufp, -1);
-    rows = bytes_read / scanline_size;
-    if ((strip < (nstrips - 1)) && (bytes_read != (int32)stripsize))
-      TIFFError("", "Strip %d: read %lu bytes, strip size %lu",
-		(int)strip + 1, (unsigned long) bytes_read, (unsigned long)stripsize);
+        if (scanline_size == 0) {
+                TIFFError("", "TIFF scanline size is zero!");    
+                return 0;
+        }
 
-    if (bytes_read < 0 && !ignore)
-      {
-      TIFFError("", "Error reading strip %lu after %lu rows",
-		(unsigned long) strip, (unsigned long)rows);
-      return 0;
-      }
-    bufp += bytes_read;
-    }
+        for (strip = 0; strip < nstrips; strip++) {
+                bytes_read = TIFFReadEncodedStrip (in, strip, bufp, -1);
+                rows = bytes_read / scanline_size;
+                if ((strip < (nstrips - 1)) && (bytes_read != (int32)stripsize))
+                        TIFFError("", "Strip %d: read %lu bytes, strip size %lu",
+                                  (int)strip + 1, (unsigned long) bytes_read,
+                                  (unsigned long)stripsize);
 
- return 1;
-  } /* end readContigStripsIntoBuffer */
+                if (bytes_read < 0 && !ignore) {
+                        TIFFError("", "Error reading strip %lu after %lu rows",
+                                  (unsigned long) strip, (unsigned long)rows);
+                        return 0;
+                }
+                bufp += bytes_read;
+        }
+
+        return 1;
+} /* end readContigStripsIntoBuffer */
 
 static int 
 combineSeparateSamplesBytes (unsigned char *srcbuffs[], unsigned char *out,
