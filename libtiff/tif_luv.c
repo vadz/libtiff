c13130d3dd1f35f787ffeae46388bec9e8d2108e
bfriesen <bfriesen>
2015-05-31 21:21:22 +0000

    (LogLuvDecodeStrip): Fix Coverity 991239 "Division or modulo by zero".

--- libtiff/tif_luv.c
+++ libtiff/tif_luv.c
@@ -379,6 +379,9 @@ LogLuvDecodeStrip(TIFF* tif, uint8* bp, tmsize_t cc, uint16 s)
 {
 	tmsize_t rowlen = TIFFScanlineSize(tif);
 
+        if (rowlen == 0)
+                return 0;
+
 	assert(cc%rowlen == 0);
 	while (cc && (*tif->tif_decoderow)(tif, bp, rowlen, s))
 		bp += rowlen, cc -= rowlen;
