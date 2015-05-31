2ee6f0871dabd85ca5cf1083d17627f0e5353172
bfriesen <bfriesen>
2015-05-31 21:24:44 +0000

    (LogLuvEncodeTile): Fix Coverity 991241 "Division or modulo by zero".

--- libtiff/tif_luv.c
+++ libtiff/tif_luv.c
@@ -668,6 +668,9 @@ LogLuvEncodeTile(TIFF* tif, uint8* bp, tmsize_t cc, uint16 s)
 {
 	tmsize_t rowlen = TIFFTileRowSize(tif);
 
+        if (rowlen == 0)
+                return 0;
+
 	assert(cc%rowlen == 0);
 	while (cc && (*tif->tif_encoderow)(tif, bp, rowlen, s) == 1)
 		bp += rowlen, cc -= rowlen;
