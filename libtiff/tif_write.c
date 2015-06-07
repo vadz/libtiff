5b4d037f0bf51b77e2c4c378c6d5f7648ea48742
bfriesen <bfriesen>
2015-06-07 23:00:23 +0000

    (TIFFWriteScanline): Fix Coverity 715979 "Division or modulo by zero".

--- libtiff/tif_write.c
+++ libtiff/tif_write.c
@@ -115,6 +115,10 @@ TIFFWriteScanline(TIFF* tif, void* buf, uint32 row, uint16 sample)
 		if (strip >= td->td_stripsperimage && imagegrew)
 			td->td_stripsperimage =
 			    TIFFhowmany_32(td->td_imagelength,td->td_rowsperstrip);
+                if (td->td_stripsperimage == 0) {
+                        TIFFErrorExt(tif->tif_clientdata, module, "Zero strips per image");
+                        return (-1);
+                }
 		tif->tif_row =
 		    (strip % td->td_stripsperimage) * td->td_rowsperstrip;
 		if ((tif->tif_flags & TIFF_CODERSETUP) == 0) {
