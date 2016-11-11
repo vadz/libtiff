3b3135aa6412425744d0a6e59227f325a148715c
erouault <erouault>
2016-11-11 21:28:24 +0000

    Fix typo in comment

--- tools/tiff2pdf.c
+++ tools/tiff2pdf.c
@@ -3697,7 +3697,7 @@ t2p_sample_rgbaa_to_rgb(tdata_t data, uint32 samplecount)
 	uint32 i;
 	
     /* For the 3 first samples, there is overlapping between souce and
-       destination, so huge memmove().
+       destination, so use memmove().
        See http://bugzilla.maptools.org/show_bug.cgi?id=2577 */
     for(i = 0; i < 3 && i < samplecount; i++)
         memmove((uint8*)data + i * 3, (uint8*)data + i * 4, 3);
