d60332057b9575ada4f264489582b13e30137be1
erouault <erouault>
2017-01-11 19:02:49 +0000

    * libtiff/tiffiop.h, tif_unix.c, tif_win32.c, tif_vms.c: add _TIFFcalloc()
    * libtiff/tif_read.c: TIFFReadBufferSetup(): use _TIFFcalloc() to zero
    initialize tif_rawdata.
    Fixes http://bugzilla.maptools.org/show_bug.cgi?id=2651

--- libtiff/tif_vms.c
+++ libtiff/tif_vms.c
@@ -270,6 +270,14 @@ _TIFFmalloc(tsize_t s)
 	return (malloc((size_t) s));
 }
 
+void* _TIFFcalloc(tmsize_t nmemb, tmsize_t siz)
+{
+    if( nmemb == 0 || siz == 0 )
+        return ((void *) NULL);
+
+    return calloc((size_t) nmemb, (size_t)siz);
+}
+
 void
 _TIFFfree(tdata_t p)
 {
